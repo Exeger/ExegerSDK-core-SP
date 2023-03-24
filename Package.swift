beec49333f1bb25426ab1d56314533f7b36a49ee
Emil Aleborn <emil.aleborn@mobileinteraction.se>
2023-03-24 13:20:25 +0100

    Release 1.2.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerfoyleCore",
-            url: "https://exeger.jfrog.io/artifactory/xcframeworks-generic-local/PowerfoyleCore/1.1.0-SNAPSHOT/PowerfoyleCore.zip",
-            checksum: "bd915b5989a88a7610f9a677c3b96490e6530aefcd8ea0be90451c274f1f2658"
+            url: "http://51.12.242.35:8081/repository/maven-releases/com/exeger/powerfoyle/core-xcframework/1.2.2/core-xcframework-1.2.2.zip",
+            checksum: "d007efee6a31dd9b4a976c38c470789c5ebf5c0d79d89d7d55b34f226416a017"
         ),
     ]
 )
