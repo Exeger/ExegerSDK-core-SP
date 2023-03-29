50be558f416db2be789a703a76ebecf5736a1fa9
Erik Hedrenius <60609589+erikhedreniusMI@users.noreply.github.com>
2023-03-29 16:28:16 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerfoyleCore",
-            url: "https://maven.exeger.com/repository/maven-releases/com/exeger/powerfoyle/core-xcframework/1.2.2/core-xcframework-1.2.2.zip",
+            url: "https://moody-donuts-make-92-33-178-98.loca.lt/core-xcframework-1.2.2.zip",
             checksum: "d007efee6a31dd9b4a976c38c470789c5ebf5c0d79d89d7d55b34f226416a017"
         ),
     ]
