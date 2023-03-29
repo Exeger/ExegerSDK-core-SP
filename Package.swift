871fa3980cdecb92d4134f8ca12e9d0367881fa3
Emil Aleborn <emil.aleborn@mobileinteraction.se>
2023-03-29 15:11:51 +0200

    Update repository url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerfoyleCore",
-            url: "http://51.12.242.35:8081/repository/maven-releases/com/exeger/powerfoyle/core-xcframework/1.2.2/core-xcframework-1.2.2.zip",
+            url: "https://maven.exeger.com/repository/maven-releases/com/exeger/powerfoyle/core-xcframework/1.2.2/core-xcframework-1.2.2.zip",
             checksum: "d007efee6a31dd9b4a976c38c470789c5ebf5c0d79d89d7d55b34f226416a017"
         ),
     ]
