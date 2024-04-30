9813715a5e0010bf5f5e80781e1c7a5eb4663a27
Erik H <erik.hedrenius@mobileinteraction.se>
2024-04-30 14:36:56 +0200

    Update version and checksum

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PowerfoyleCore",
-            url: "https://pkgs.dev.azure.com/exeger/PowerfoyleSDK/_packaging/repository/maven/v1/com.exeger.powerfoyle/core-xcframework/1.2.4/core-xcframework-1.2.4.zip",
-            checksum: "c1626c039bc0fa834bc2154e7c2d3c2153cf0e330808d0415c9e69d2e39b512c"
+            url: "https://pkgs.dev.azure.com/exeger/PowerfoyleSDK/_packaging/repository/maven/v1/com.exeger.powerfoyle/core-xcframework/2.0/core-xcframework-2.0.zip",
+            checksum: "c90b053f49d3985d3cef130d77d16e2223524608cdea5de93db3e9892b2fee56"
         ),
     ]
 )
